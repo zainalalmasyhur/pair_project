02ad964a2ad7d5b58540335d1ea9e585a896927f
zainalalmasyhur <zainalalmasyhur30@gmail.com>
2024-05-22 17:40:04 +0700

    hapus folder view&router

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const router = require(`./routers`)
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,"public")))
+
+
+app.use('/', router)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
