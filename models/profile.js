c520b24ab2799b7d4326f13fbfa4013574289d9f
zainalalmasyhur <zainalalmasyhur30@gmail.com>
2024-05-23 17:03:54 +0700

    backup

--- models/profile.js
+++ models/profile.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+const { use } = require('../routers');
 module.exports = (sequelize, DataTypes) => {
   class Profile extends Model {
     /**
@@ -13,6 +14,53 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       this.belongsTo(models.User)
     }
+
+    get timeAgo() {
+    const seconds = Math.floor((new Date() - this.createdAt) / 1000);
+
+    const interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years ago";
+    }
+    if (interval === 1) {
+        return interval + " year ago";
+    }
+
+    const months = Math.floor(seconds / 2628000);
+    if (months > 1) {
+        return months + " months ago";
+    }
+    if (months === 1) {
+        return months + " month ago";
+    }
+
+    const days = Math.floor(seconds / 86400);
+    if (days > 1) {
+        return days + " days ago";
+    }
+    if (days === 1) {
+        return days + " day ago";
+    }
+
+    const hours = Math.floor(seconds / 3600);
+    if (hours > 1) {
+        return hours + " hours ago";
+    }
+    if (hours === 1) {
+        return hours + " hour ago";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    if (minutes > 1) {
+        return minutes + " minutes ago";
+    }
+    if (minutes === 1) {
+        return minutes + " minute ago";
+    }
+
+    return "just now";
+}
 
   }
   Profile.init({
@@ -28,5 +76,15 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'Profile',
   });
+
+  Profile.beforeCreate(async (user, options) => {
+    if (!user.imgProfile) {
+      user.imgProfile = 'https://t4.ftcdn.net/jpg/00/64/67/27/360_F_64672736_U5kpdGs9keUll8CRQ3p3YaEv2M6qkVY5.jpg'
+    }
+   user.followers = 0
+   user.following = 0
+   user.posts = 0
+  });
+
   return Profile;
 };
--- routers/index.js
+++ routers/index.js
@@ -19,15 +19,6 @@ router.post("/signup", Controller.postUserSignup);
 // --- Log-Out
 router.get("/logout", Controller.logOut);
 
-router.use(function (req,res,next){
-    if (!req.session.username) {
-        const err = 'tolong login terlebih dahulu'
-        res.redirect(`/login?error=${err}`)
-    } else {
-        next()
-    }
-})
-
 router.use("/admin", admin);
 router.use("/user", user);
 
